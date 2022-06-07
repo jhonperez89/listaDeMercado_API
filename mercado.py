1e608ad655df5a3b97213f833ad03859035aa91e
jhon david <pjhon2020@gmail.com>
2022-06-06 21:50:45 -0500

    commit 2 con err404

--- mercado.py
+++ mercado.py
@@ -6,60 +6,82 @@ app = Flask(__name__)
 
 productos = []
 
-@app.route("/")
+@app.errorhandler(404) # Mustra el error 404 en una pagina html personalizada 
+def pagina_no_encontrada(e):
+    return render_template('404.html'),404
+
+
+
+@app.route("/") # Mustra todos los productos que han sido agregados
 def obtenerProductos():
     try:
         return jsonify({"Productos" : productos})
     except:
-        return render_template('404.html'), 404
+        return 'Verifica tus datos ha ocurrido un error'
+       
+        
 
-@app.route("/producto/<string:nombreProducto>", methods=['GET'])
+@app.route("/producto/<string:nombreProducto>", methods=['GET']) # Mustra un solo producto por su nombre
 def obtenerProducto(nombreProducto):
+    try:
+        devuelveProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
+        if (len(devuelveProducto) > 0):
+            return jsonify({"producto" : devuelveProducto[0]})
+        return 'Producto NO encontrado'
+    except:
+        return 'Verifica tus datos ha ocurrido un error'
     
-    devuelveProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
-    if (len(devuelveProducto) > 0):
-        return jsonify({"producto" : devuelveProducto[0]})
-    return 'Producto NO encontrado'
 
-@app.route("/agregar", methods=['POST'])
-def agregar():
-    nuevosProductos = {
-        "nombre" : request.json['nombre'],
-        "cantidad" : request.json['cantidad'],
-        "precio" : request.json['precio']
-    }
-    productos.append(nuevosProductos)
-    return jsonify({
-            "mensaje": "Exito agregando producto",
-            "productos": nuevosProductos
-        })
+@app.route("/agregar", methods=['POST']) # Al agregar productos debe hacerce por body 
+def agregar():                           # y en formato json en thunder client o postman
+    try:
+        nuevosProductos = {
+            "nombre" : request.json['nombre'],
+            "cantidad" : request.json['cantidad'],
+            "precio" : request.json['precio']
+        }
+        productos.append(nuevosProductos)
+        return jsonify({
+                "mensaje": "Exito agregando producto",
+                "productos": nuevosProductos
+                
+            })
+    except:
+        return 'Verifica tus datos ha ocurrido un error'
+    
+    
 
 
-@app.route("/editar/<string:nombreProducto>", methods=['PUT'])
+@app.route("/editar/<string:nombreProducto>", methods=['PUT']) # edita los productos mediante un json 
 def editar(nombreProducto):
-    cambioProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
-    if (len(cambioProducto) > 0):
-        cambioProducto[0]['nombre'] = request.json['nombre']
-        cambioProducto[0]['cantidad'] = request.json['cantidad']
-        cambioProducto[0]['precio'] = request.json['precio'] 
-        return jsonify({
-            "mensaje" : "Exito editando el producto",
-            "producto" : cambioProducto[0]
-        })
-    return 'producto NO encontrado'
+    try:
+        cambioProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
+        if (len(cambioProducto) > 0):
+            cambioProducto[0]['nombre'] = request.json['nombre']
+            cambioProducto[0]['cantidad'] = request.json['cantidad']
+            cambioProducto[0]['precio'] = request.json['precio'] 
+            return jsonify({
+                "mensaje" : "Exito editando el producto",
+                "producto" : cambioProducto[0]
+            })
+        return 'producto NO encontrado'
+    except:
+        return 'Verifica la informacion ha ocurrido un error'
 
 
 
-@app.route("/eliminar/<string:nombreProducto>", methods=['DELETE'])
-def eliminar(nombreProducto):
-    eliminarProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
-    if len(eliminarProducto) > 0:
-        productos.remove(eliminarProducto[0])
-        return jsonify({
-            "mensaje": "Producto Eliminado",
-            "productos": productos
-        })
-    return "Producto NO encontrado"
-
+@app.route("/eliminar/<string:nombreProducto>", methods=['DELETE']) # selecciona el nombre del producto 
+def eliminar(nombreProducto):                                       # que desea eliminar indicando el nombre
+    try:
+        eliminarProducto = [producto for producto in productos if producto['nombre'] == nombreProducto]
+        if len(eliminarProducto) > 0:
+            productos.remove(eliminarProducto[0])
+            return jsonify({
+                "mensaje": "Producto Eliminado",
+                "productos": productos
+            })
+        return "Producto NO encontrado"
+    except:
+        return 'Verifica la informacion ha ocurrido un error'
 
 
